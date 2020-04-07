d2994c8bab55956a101574a91b2e3710cb36cad1
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-07 13:03:44 -0700

    uncommented the slow solution

--- genParen.js
+++ genParen.js
@@ -2,59 +2,59 @@
 //  * @param {number} n
 //  * @return {string[]}
 //  */
-// const generateParenthesis =  (n) => {
-//     // brute force super duper slow
-//     // need backtracking solution
-//     let parenStr = ""
-//     for (let i=0; i < n; i++) {
-//         parenStr = "(" + parenStr + ")"
-//     }
+const generateParenthesis =  (n) => {
+    // brute force super duper slow
+    // need backtracking solution
+    let parenStr = ""
+    for (let i=0; i < n; i++) {
+        parenStr = "(" + parenStr + ")"
+    }
 
-//     let set = new Set()
+    let set = new Set()
 
-//     return permuteParen(parenStr, "", [], set)
+    return permuteParen(parenStr, "", [], set)
 
-// };
+};
 
-// const permuteParen = (parenStr, parenPerm = "", coll = [], set) => {
+const permuteParen = (parenStr, parenPerm = "", coll = [], set) => {
 
-//     if (!parenStr) {
-//         if (isValid(parenPerm) && !set.has(parenPerm)) {
-//             set.add(parenPerm)
-//             coll.push(parenPerm)
-//         }
-//         return
-//     }
+    if (!parenStr) {
+        if (isValid(parenPerm) && !set.has(parenPerm)) {
+            set.add(parenPerm)
+            coll.push(parenPerm)
+        }
+        return
+    }
 
-//     if (isValid(parenPerm)) {
-//         for (let i = 0; i < parenStr.length; i++) {
-//             let original = parenPerm
-//             parenPerm += parenStr[i]
-//             permuteParen(parenStr.slice(0, i) + parenStr.slice(i + 1), parenPerm, coll, set)
-//             parenPerm = original
-//         }
-//     }
+    if (isValid(parenPerm)) {
+        for (let i = 0; i < parenStr.length; i++) {
+            let original = parenPerm
+            parenPerm += parenStr[i]
+            permuteParen(parenStr.slice(0, i) + parenStr.slice(i + 1), parenPerm, coll, set)
+            parenPerm = original
+        }
+    }
 
-//     return coll
-// }
+    return coll
+}
 
-// const isValid = (s) => {
-//     if (!s) return true
-//     const store = []
+const isValid = (s) => {
+    if (!s) return true
+    const store = []
 
-//     for (let i = 0; i < s.length; i++) {
-//         if (s[i] === "(") {
-//             store.push(s[i])
-//         }
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            store.push(s[i])
+        }
 
-//         if (s[i] === ")") {
-//             let popped = store.pop()
-//             if (popped !== "(") return false
-//         }
-//     }
-//     if (store.length > 0) return false
-//     return true
-// };
+        if (s[i] === ")") {
+            let popped = store.pop()
+            if (popped !== "(") return false
+        }
+    }
+    if (store.length > 0) return false
+    return true
+};
 
 
 
