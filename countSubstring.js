9cef37c53c392955f76498eadc1e6e149031bb69
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-09 13:47:11 -0700

    started solving the problem

---
+++ countSubstring.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const countSubstrings = s => {
+    
+    let palindromeCount = 0
+
+    for (let i=0;i<s.length;i++) {
+
+        
+
+    }
+
+
+    return palindromeCount
+};
+
+
+
+let s = "abc"
+console.log(countSubstrings(s), 3)
+// Explanation: Three palindromic strings: "a", "b", "c".
+ 
+
+s = "aaa"
+console.log(countSubstrings(s), 6)
+// Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
