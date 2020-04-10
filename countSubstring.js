130d67f1ff991bf6a4eb7f97f051eb4b8115f526
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-09 18:52:31 -0700

    working solution for counting palindromes

--- countSubstring.js
+++ countSubstring.js
@@ -6,9 +6,29 @@ const countSubstrings = s => {
     
     let palindromeCount = 0
 
-    for (let i=0;i<s.length;i++) {
+    for (let i=0; i<s.length;i++) {
 
-        
+        palindromeCount++
+
+        let start = i
+        let end = i+1
+
+        while (s[end] !== undefined && s[start] !== undefined && s[start] === s[end] ) {
+            //keep expanding the palindrome
+            palindromeCount++
+            start--
+            end++
+        }
+
+        start = i-1
+        end = i+1
+
+        while(s[end] !== undefined && s[start] !== undefined && s[start] === s[end] ) {
+            //keep expanding the palindrome
+            palindromeCount++
+            start--
+            end++
+        }
 
     }
 
@@ -16,7 +36,14 @@ const countSubstrings = s => {
     return palindromeCount
 };
 
+/*
 
+sample strings:
+
+      i
+ABBACABBA
+
+*/
 
 let s = "abc"
 console.log(countSubstrings(s), 3)
@@ -26,3 +53,7 @@ console.log(countSubstrings(s), 3)
 s = "aaa"
 console.log(countSubstrings(s), 6)
 // Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+
+s = "abbacabba"
+console.log(countSubstrings(s),17)
